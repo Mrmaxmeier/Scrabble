16f9379113e43de5f979d32263c7ea0283be57c3
Milof98 <milan.valentin@live.de>
2016-01-25 14:53:27 +0100

    First Commit. Adding the libgdx projects for eclipse

---
+++ desktop/src/de/dingermueller/chess/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.dingermueller.chess.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.dingermueller.chess.ChessMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ChessMain(), config);
+	}
+}
