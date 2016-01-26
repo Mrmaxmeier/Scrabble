966c29d273b4bb458ed381e9caf2a9f172f4b6a9
Milan Müller <milan.valentin@live.de>
2016-01-26 16:19:08 +0100

    Dahltest

--- core/src/de/dingermueller/chess/ChessMain.java
+++ core/src/de/dingermueller/chess/ChessMain.java
@@ -14,6 +14,7 @@ public class ChessMain extends ApplicationAdapter {
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		System.out.println("Dahl");
 	}
 
 	@Override
