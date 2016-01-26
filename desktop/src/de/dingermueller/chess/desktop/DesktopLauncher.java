5d4c9d707d33e282753d70e6f2f13f632849ce32
Milan Müller <milan.valentin@live.de>
2016-01-26 16:49:46 +0100

    Fenstertitel

--- desktop/src/de/dingermueller/chess/desktop/DesktopLauncher.java
+++ desktop/src/de/dingermueller/chess/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import de.dingermueller.chess.ChessMain;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Chess";
+		config.width= 800;
+		config.height = 800;
 		new LwjglApplication(new ChessMain(), config);
 	}
 }
